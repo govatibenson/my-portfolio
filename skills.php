bf6fe7ea5e12b41905c666bab4e62c35d6e6667a
govatibenson <132843899+govatibenson@users.noreply.github.com>
2023-06-09 16:11:03 +0200

    Update skills.php

--- skills.php
+++ skills.php
@@ -14,14 +14,32 @@
 <h2 text align="center"><a href="index.html" >HOME</a></h2>
 </head>
 <body>
+	<?php
+$servernane="localhost";
+$username="root";
+$password="";
+$database="skills";
+
+$conn=myqli_connect($servername,$username,$password,$database);
+if($conn){
+echo "connected";
+}
+else{
+echo "not connected";
+}
+$query=mysqli_query($conn,"select*from my_skill");
+
+while($row=mysqli_fetch_array(query)){
+echo $row["id"]."\t".$row["skill"]."<br>";
+}
+?>
 <h4>SKILLS, TOOLS AND TECHNOLOGIES</h4> <br>
 <P3>Below are the skills, tools and technologies that I use in developing my projects.</P3><br>
 <ol>
 				<ul>
-					<li>HTML(Hypertext Markup Language</li>
-					<li>CSS(Cascading Style Sheets)</li>
-					<li>Sublime Text</li>
-					<li>Version Control(Git)</li>
+					<li>Coding using C and Java programming languages</li>
+					<li>Web designing and development</li>
+					<li>interacting with Github </li>
 				</ul>
 			</ol>
 <UL>
